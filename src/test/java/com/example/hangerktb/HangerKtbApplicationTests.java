1d0bbf4d4cf53e310f0102eac58ce174a3561fdb
Kramer Brown <brownkramer63@gmail.com>
2023-03-17 16:11:08 -0500

    Initial commit

---
+++ src/test/java/com/example/hangerktb/HangerKtbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hangerktb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangerKtbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
